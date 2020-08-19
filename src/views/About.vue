221e4c6db17670c7ee748499f403612b97ae8ce9
Sumer Patel <sumerdpatel@gmail.com>
2020-08-19 15:50:50 +0530

    added info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" id="about">
+    <div class="about-title">
+      <h1>Taivaas</h1>
+    </div>
+    <div class="about-para">
+
+    </div>
+    <div class="about-foot">
+      <p>All Rights Reserved.</p> 
+    </div>
   </div>
 </template>
+
+<style scoped>
+.about{
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+}
+</style>
