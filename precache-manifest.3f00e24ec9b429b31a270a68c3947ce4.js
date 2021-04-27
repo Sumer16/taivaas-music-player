2da843ab0c8a51f067beed0e0294f4a99de8a160
Sumer Patel <sumerdpatel@gmail.com>
2021-04-27 13:56:39 +0530

    deploy

---
+++ precache-manifest.3f00e24ec9b429b31a270a68c3947ce4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e47f7dbef1d3b008d1c",
+    "url": "/taivaas-music-player/css/app.8519d733.css"
+  },
+  {
+    "revision": "ced213fb834ae04058d20c73d4675d6b",
+    "url": "/taivaas-music-player/img/thumbnail-1.ced213fb.jpg"
+  },
+  {
+    "revision": "fc2290e1fd0dc7601dd992d06ed0aaf8",
+    "url": "/taivaas-music-player/img/thumbnail-2.fc2290e1.jpg"
+  },
+  {
+    "revision": "7e2dfde198b75087829c676d351f12db",
+    "url": "/taivaas-music-player/img/thumbnail-3.7e2dfde1.jpg"
+  },
+  {
+    "revision": "c0d96e343981943a03293dad409edb90",
+    "url": "/taivaas-music-player/img/thumbnail-4.c0d96e34.jpg"
+  },
+  {
+    "revision": "64926f5ddee25ab7c2c6edecc8efca69",
+    "url": "/taivaas-music-player/img/thumbnail-5.64926f5d.jpg"
+  },
+  {
+    "revision": "1e5dc21531c4fda43187f841c51f9290",
+    "url": "/taivaas-music-player/index.html"
+  },
+  {
+    "revision": "1e47f7dbef1d3b008d1c",
+    "url": "/taivaas-music-player/js/app.40696b33.js"
+  },
+  {
+    "revision": "22b82b405a6f8683cdd8",
+    "url": "/taivaas-music-player/js/chunk-vendors.c239c4c4.js"
+  },
+  {
+    "revision": "c60e5b44d2c24a79833df9006c15bd13",
+    "url": "/taivaas-music-player/manifest.json"
+  },
+  {
+    "revision": "66d9afbe550673498c13d37b7efd95d0",
+    "url": "/taivaas-music-player/media/affection.66d9afbe.mp3"
+  },
+  {
+    "revision": "96cacaed8b0ac954917eda7759a6724a",
+    "url": "/taivaas-music-player/media/apocalypse.96cacaed.mp3"
+  },
+  {
+    "revision": "d3df2025c58417215f691ee464a98f3e",
+    "url": "/taivaas-music-player/media/boom-clap.d3df2025.mp3"
+  },
+  {
+    "revision": "fa76e269a96cec07e47e587528cfa23e",
+    "url": "/taivaas-music-player/media/photograph.fa76e269.mp3"
+  },
+  {
+    "revision": "8ebc1919d38e49632034142def502e3d",
+    "url": "/taivaas-music-player/media/wake-me-up.8ebc1919.mp3"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/taivaas-music-player/robots.txt"
+  }
+]);
